e92ec02188b89b1edc663fda2bbfc200fc9c4d4c
Colin Lynch <61156429+colinmichaellynch@users.noreply.github.com>
2022-12-29 16:19:03 -0700

    Update simulateExperiments.m

--- simulateExperiments.m
+++ simulateExperiments.m
@@ -3,7 +3,7 @@ clear
 tic
 
 y = 10; 
-n_max = 12; 
+n_max = 20; 
 delta_threshold = .3; 
 alpha = .05;
 lower_delta_val = .00001; 
@@ -149,3 +149,66 @@ for n = 1:n_max
 end
 
 time = toc;
+
+counter = 0; 
+for n = 1:n_max        
+    for j = 1:n_max
+        for o = 1:sims_number_of_parameters
+            for u = 1:sims_number_of_parameters
+                for q = 1:sims_number_of_parameters
+                    for i = 1:sims_iterate_parameters
+                        counter = counter+1; 
+                        full_colony_vec = nonzeros(squeeze(full_colony_tens(n, j, o, u, q, i, :)));
+                        halfway_point = round(length(full_colony_vec)/2); 
+                        full_colony_a = full_colony_vec(1:halfway_point); 
+                        full_colony_b = full_colony_vec(halfway_point+1:end); 
+                        delta_estimate_vec(counter) = mean(full_colony_b) - mean(full_colony_a); 
+                        
+                        if isnan(var(full_colony_vec))
+                            best_distribution{counter} = NaN; 
+                        else
+                            test_cdf1 = makedist('tlocationscale','mu',mean(full_colony_vec),'sigma',var(full_colony_vec)+.0001,'nu',1);
+                            test_cdf2 = makedist('Normal','mu',mean(full_colony_vec),'sigma',var(full_colony_vec)+.0001); 
+                            [~, ~, ksstat1] = kstest(full_colony_vec,'CDF',test_cdf1); 
+                            [~, ~, ksstat2] = kstest(full_colony_vec,'CDF',test_cdf2); 
+                            if ksstat1 > ksstat2
+                                best_distribution{counter} = "Normal";
+                            else
+                                best_distribution{counter} = "T";
+                            end
+                            
+                            difference_between_dist(counter) = ksstat2-ksstat1; 
+                            
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+delta_error = (delta_tens(:) - delta_estimate_vec'); 
+delta_estimate_vec=delta_estimate_vec'; 
+
+matrix = [var_value_tens(:), false_neg_rate_tens(:), true_pos_rate_tens(:), false_pos_rate_tens(:), true_neg_rate_tens(:), ...
+    false_neg_rate_type2_tens(:), true_pos_rate_type2_tens(:), false_pos_rate_type2_tens(:), true_neg_rate_type2_tens(:),...
+    within_colony_std_treat_a_tens(:), between_colony_std_treat_a_tens(:), within_colony_std_treat_b_tens(:), between_colony_std_treat_b_tens(:), within_colony_std_treat_b_type_2_tens(:)...
+    between_colony_std_treat_b_type_2_tens(:), accuracy_tens(:), accuracy_type2_tens(:), balanced_accuracy_tens(:), balanced_accuracy_type2_tens(:),...
+    within_colony_rep_tens(:), between_colony_rep_tens(:), inter_var_tens(:), intra_var_tens(:), delta_tens(:),...
+    delta_estimate_vec, delta_error]; 
+
+headers = {'Variance' 'False_Neg_Rate' 'True_Pos_Rate' 'False_Pos_Rate' 'True_Neg_Rate' 'False_Neg_Rate_Type2' 'True_Pos_Rate_Type2' 'False_Pos_Rate_Type2' 'True_Neg_Rate_Type2'...
+    'Within_Colony_std_treat_a' 'Between_Colony_std_treat_a' 'Within_Colony_std_treat_b' 'Between_Colony_std_treat_b' 'Within_Colony_std_treat_b_type2' 'Between_Colony_std_treat_b_type2'...
+    'Accuracy', 'Accuracy_Type2','Balanced_Accuracy', 'Balanced_Accuracy_Type2', 'Within_colony_rep', 'between_colony_rep', 'inter_var', 'intra_var', 'delta', 'delta_estimate', 'delta_error'};
+
+colony_sampling_final = [headers;num2cell(matrix)];
+
+%export as csv
+
+fid = fopen('colony_sampling_final.csv', 'w');
+fprintf(fid, '%s,', colony_sampling_final{1,1:end-1});
+fprintf(fid, '%s\n', colony_sampling_final{1,end});
+fclose(fid);
+dlmwrite('colony_sampling_final.csv', colony_sampling_final(2:end,:), '-append');
+    
+
